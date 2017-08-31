e95f8ce3f11e60aa81f7a583f7d2500609dea91a
Choadpet <po15f94vetal>
2017-08-31 17:39:46 +0300

    create map, copy into map instruction from each player

--- cmake-build-debug/CMakeFiles/corewar.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/corewar.dir/cmake_clean.cmake
@@ -3,7 +3,7 @@ file(REMOVE_RECURSE
   "CMakeFiles/corewar.dir/vp_virtual_machine/vp_err_u.c.o"
   "CMakeFiles/corewar.dir/vp_virtual_machine/vp_basic.c.o"
   "CMakeFiles/corewar.dir/vp_virtual_machine/vp_commands.c.o"
-  "CMakeFiles/corewar.dir/vp_virtual_machine/op.c.o"
+  "CMakeFiles/corewar.dir/op.c.o"
   "corewar.pdb"
   "corewar"
 )
--- vp_virtual_machine/vp_commands.c
+++ vp_virtual_machine/vp_commands.c
@@ -12,42 +12,52 @@
 
 #include "virtualm.h"
 
-void	prog_commands(t_skrr *skrr)
+void		prog_commands(t_skrr *skrr, int argc)
 {
-	char 			*line;
-	unsigned int 	cmd[1];
+	char	*line;
 
 	line = NULL;
-	(get_next_line(skrr->fd, &line) < 0) ? exit(0) : 0;
-	cmd[0] = (unsigned int)line[0];
-	ft_printf("\nCommands! Player %d\n", skrr->j);
-	(cmd[0] == 1) ? ft_printf("live") : 0;
-	(cmd[0] == 2) ? ft_printf("ld") : 0;
-	(cmd[0] == 3) ? ft_printf("st") : 0;
-	(cmd[0] == 4) ? ft_printf("add") : 0;
-	(cmd[0] == 5) ? ft_printf("sub") : 0;
-	(cmd[0] == 6) ? ft_printf("and") : 0;
-	(cmd[0] == 7) ? ft_printf("or") : 0;
-	(cmd[0] == 8) ? ft_printf("xor") : 0;
-	(cmd[0] == 9) ? zjmp(skrr, cmd[0] - 1) : 0;
-	(cmd[0] == 10) ? ft_printf("ldi") : 0;
-	(cmd[0] == 11) ? ft_printf("sti") : 0;
-	(cmd[0] == 12) ? ft_printf("fork") : 0;
-	(cmd[0] == 13) ? ft_printf("lld") : 0;
-	(cmd[0] == 14) ? ft_printf("lldi") : 0;
-	(cmd[0] == 15) ? ft_printf("lfork") : 0;
-	(cmd[0] == 16) ? ft_printf("aff") : 0;
-	ft_printf("\n");
+	player_position(skrr, argc - 1);
+	(skrr->flag == -1) ? init_map(skrr) : 0;
+	if ((get_next_line(skrr->fd, &line) < 0))
+		exit (1);
+	ft_strcpy(&skrr->map[skrr->player_pos], line);
 }
 
-int 	zjmp(t_skrr *skrr, int i)
+void	print_map(t_skrr *skrr)
 {
-	ft_printf("name: %s\n", op_tab[i].name);
-	ft_printf("number_of_arg: %u\n", op_tab[i].numb_of_arg);
-	ft_printf("arg: %x\n", op_tab[i].arg[0]);
-	ft_printf("opcode: %u\n", op_tab[i].opcode);
-	ft_printf("cycles: %u\n", op_tab[i].cycles);
-	ft_printf("codage: %u\n", op_tab[i].codage);
-	ft_printf("wtf: %u\n", op_tab[i].wtf);
-	return (1);
+	skrr->i = 0;
+	ft_printf("\n");
+	while (skrr->i < 4096)
+	{
+		if ((skrr->i) % 1365 == 0)
+			ft_printf(RED"%.2x "RESET, skrr->map[skrr->i]);
+		else
+			ft_printf("%hh.2x ", skrr->map[skrr->i]);
+		((skrr->i + 1) % 64 == 0) ? ft_printf("\n") : 0;
+		skrr->i++;
+	}
+}
+
+static void	init_map(t_skrr *skrr)
+{
+	skrr->i = 0;
+	while (skrr->i < 4096)
+	{
+		skrr->map[skrr->i] = 0;
+		skrr->i++;
+	}
+	skrr->flag = 1;
+}
+
+static void	player_position(t_skrr *skrr, int argc)
+{
+	int tmp;
+
+	tmp = 0;
+	(skrr->j == 1) ? skrr->player_pos = 0 : 0;
+	(skrr->j != 1) ? tmp = (MEM_SIZE / argc) : 0;
+	(skrr->j == 2) ? skrr->player_pos += tmp : 0;
+	(skrr->j == 3) ? skrr->player_pos = tmp * 2 : 0;
+	(skrr->j == 4) ? skrr->player_pos = tmp * 3 : 0;
 }
