82b1d365d9590b25ef5bc3fa120a031bfa63f45f
d-spiridonov <dspyrydo@student.unit.ua>
2017-09-25 20:59:40 +0300

    started experimenting with windows

--- cor/visualisation/ds_visualize.c
+++ cor/visualisation/ds_visualize.c
@@ -2,4 +2,25 @@
 // Created by Dmytrii Spyrydonov on 9/24/17.
 //
 
-#include <ncurses.h>
+#include "../../corewar.h"
+
+int main()
+{
+	initscr();
+	start_color();
+	WINDOW *code;
+	WINDOW *menu;
+	code = newwin(getmaxy(stdscr), getmaxx(stdscr) - 20, 10, 10);
+	menu = newwin(getmaxy(stdscr), 20, 50, getmaxx(stdscr) - 20);
+	box(code, 0, 0);
+	box(menu, 0, 0);
+	init_pair(1, COLOR_BLUE, COLOR_BLUE);
+	wattron(code, COLOR_PAIR(1));
+	wprintw(code, "****************");
+	wprintw(menu, "****************");
+	attroff(1);
+	wgetch(code);
+	wgetch(menu);
+	endwin();
+	return (0);
+}
