b1ec3bc266d4e5b82c63871ddbc5769212a472b4
d-spiridonov <dspyrydo@student.unit.ua>
2017-10-07 17:39:12 +0300

    boxes are ready

--- cor/visualisation/ds_visualize.c
+++ cor/visualisation/ds_visualize.c
@@ -7,20 +7,18 @@
 int main()
 {
 	initscr();
+	int	startx, starty, width, height;
+	getmaxyx(stdscr, height, width);
 	start_color();
 	WINDOW *code;
 	WINDOW *menu;
-	code = newwin(getmaxy(stdscr), getmaxx(stdscr) - 20, 10, 10);
-	menu = newwin(getmaxy(stdscr), 20, 50, getmaxx(stdscr) - 20);
+	code = newwin(height, width - 50, 0, 0);
+	menu = newwin(height, 49, 0, width - 49);
 	box(code, 0, 0);
 	box(menu, 0, 0);
-	init_pair(1, COLOR_BLUE, COLOR_BLUE);
-	wattron(code, COLOR_PAIR(1));
-	wprintw(code, "****************");
-	wprintw(menu, "****************");
-	attroff(1);
+	wrefresh(code);
+	wrefresh(menu);
 	wgetch(code);
-	wgetch(menu);
 	endwin();
 	return (0);
 }
