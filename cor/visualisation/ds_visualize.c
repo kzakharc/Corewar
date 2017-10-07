aa77323e7fa533d5a1dcf93046da3d4714865ac2
d-spiridonov <dspyrydo@student.unit.ua>
2017-10-07 19:41:58 +0300

    beautiful CYAN boxes are now displayed

--- cor/visualisation/ds_visualize.c
+++ cor/visualisation/ds_visualize.c
@@ -4,6 +4,36 @@
 
 #include "../../corewar.h"
 
+void printmargins(WINDOW *code, WINDOW *menu, int width, int height)
+{
+	int i;
+
+	i = 0;
+	start_color();
+	init_pair(1, COLOR_CYAN, COLOR_BLACK);
+	wattron(code, COLOR_PAIR(1));
+	wattron(menu, COLOR_PAIR(1));
+	while (i < height)
+	{
+		mvwaddch(code, i, 0, ' ' | A_REVERSE);
+		mvwaddch(code, i, width - 61, ' ' | A_REVERSE);
+		mvwaddch(menu, i++, 59, ' ' | A_REVERSE);
+	}
+	i = 0;
+	while (i < width - 61)
+	{
+		mvwaddch(code, 0, i, ' ' | A_REVERSE);
+		mvwaddch(code, height - 1, i++, ' ' | A_REVERSE);
+	}
+	i = 0;
+	while (i < 61)
+	{
+		mvwaddch(menu, 0, i, ' ' | A_REVERSE);
+		mvwaddch(menu, height - 1, i++, ' ' | A_REVERSE);
+	}
+	attroff(1);
+}
+
 int main()
 {
 	initscr();
@@ -12,10 +42,11 @@ int main()
 	start_color();
 	WINDOW *code;
 	WINDOW *menu;
-	code = newwin(height, width - 50, 0, 0);
-	menu = newwin(height, 49, 0, width - 49);
-	box(code, 0, 0);
-	box(menu, 0, 0);
+	code = newwin(height, width - 60, 0, 0);
+	menu = newwin(height, 60, 0, width - 60);
+//	box(code, 0, 0);
+//	box(menu, 0, 0);
+	printmargins(code, menu, width, height);
 	wrefresh(code);
 	wrefresh(menu);
 	wgetch(code);
