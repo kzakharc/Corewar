ad52d5b174ef0d9fc2f5c4beb7a304a503e1ea49
Choadpet <po15f94vetal>
2017-10-16 16:06:36 +0300

    fixed segfault and added + MEM_SIZE to lfork instr

--- cor/virtual_machine/vp_get_winner.c
+++ cor/virtual_machine/vp_get_winner.c
@@ -21,30 +21,40 @@ void	winner(t_chmp *chmp, t_skrr *skrr)
 	chmp_tmp = chmp;
 	best_cycle = chmp_tmp->last_live;
 	best_player = chmp_tmp->ac;
-	chmp_tmp = chmp_tmp->next;
 	print_info(skrr, skrr->chmp);
-	if (skrr->max_player == 1 && (chmp_tmp))
+	if (skrr->max_player == 1)
 		ft_printf("Contestant %ld, " GRN"\"%s\", "RESET "has won !\n",
-				  chmp_tmp->id * (-1), chmp_tmp->header.prog_name);
+				  chmp_tmp->ac * (-1), chmp_tmp->header.prog_name);
 	else if (skrr->max_player > 1)
 	{
+		chmp_tmp = chmp_tmp->next;
 		while (chmp_tmp)
 		{
-			if (best_cycle < chmp_tmp->last_live)
+			if ((best_cycle <= chmp_tmp->last_live) && (best_player > chmp_tmp->ac))
+			{
 				best_cycle = chmp_tmp->last_live;
-			else if ()
+				best_player = chmp_tmp->ac;
+			}
 			chmp_tmp = chmp_tmp->next;
 		}
+		multipl_winners(skrr, best_cycle, best_player);
 	}
-	if (skrr->flag_v == 1)
-		endwin();
+	(skrr->flag_v == 1) ? endwin() : 0;
 	exit(1);
 }
 
-int 	multipl_winners(t_skrr *skrr, long *best_cycles, int i)
+void 	multipl_winners(t_skrr *skrr, long best_cycles, int best_player)
 {
+	t_chmp *chmp_tmp;
 
-	return (1);
+	chmp_tmp = skrr->chmp;
+	while (chmp_tmp)
+	{
+		if ((best_cycles == chmp_tmp->last_live) && (best_player == chmp_tmp->ac))
+			ft_printf("Contestant %ld, " GRN"\"%s\", "RESET "has won !\n",
+					  chmp_tmp->ac * (-1), chmp_tmp->header.prog_name);
+		chmp_tmp = chmp_tmp->next;
+	}
 }
 
 int 	init_lives(t_proc *process, t_skrr *skrr)
@@ -64,19 +74,13 @@ int 	init_lives(t_proc *process, t_skrr *skrr)
 		(proc_tmp->live_count > 0) ? proc_tmp->live_count = 0 : 0;
 		proc_tmp = proc_tmp->next;
 	}
-	if (skrr->nbr_live >= NBR_LIVE)
-	{
+	if (skrr->nbr_live >= NBR_LIVE && (skrr->max_checks = MAX_CHECKS))
 		skrr->cycle_to_die -= CYCLE_DELTA;
-		skrr->max_checks = MAX_CHECKS;
-	}
 	else
 	{
 		skrr->max_checks--;
-		if (skrr->max_checks == 0)
-		{
-			skrr->max_checks = MAX_CHECKS;
+		if (skrr->max_checks == 0 && (skrr->max_checks = MAX_CHECKS))
 			skrr->cycle_to_die -= CYCLE_DELTA;
-		}
 	}
 	g_ctd = 0;
 	skrr->nbr_live = 0;
